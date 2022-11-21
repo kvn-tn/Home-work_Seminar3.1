4530293cf3d60840aa6eecc033457551e3bd3e25
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-21 18:38:36 +0300

    update 2 task8

--- HomeTask/Task8/Program.cs
+++ HomeTask/Task8/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Введите число N =");
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N =");
 int N = Convert.ToInt32(Console.ReadLine());
 
 int count = 2;
