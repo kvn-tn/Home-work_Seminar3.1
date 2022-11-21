668307de145a88a4dc738914a390c453826f6555
Tatiana Kvon <kvn-tn@mail.ru>
2022-11-21 18:37:23 +0300

    Task8

---
+++ HomeTask/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число N =");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 2;
+
+while (count<=N)
+{
+    Console.WriteLine(count);
+    count+=2;
+    // count=count+2;
+}
